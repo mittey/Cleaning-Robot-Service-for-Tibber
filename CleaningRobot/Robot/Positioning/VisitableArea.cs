36b29c4852b8b9b807e58567881a94c90776166e
Dmitry Kulikov <d.k.kulikov@gmail.com>
2024-02-14 15:47:03 +0100

    Simplify more.

--- CleaningRobot/Robot/Positioning/VisitableArea.cs
+++ CleaningRobot/Robot/Positioning/VisitableArea.cs
@@ -1,30 +1,30 @@
 // ReSharper disable NotAccessedPositionalProperty.Global
 
+using System.Numerics;
+
 namespace CleaningRobot.Robot.Positioning;
 
-internal readonly record struct GridKey(int X, int Y);
-
-internal static class GridKeyExtensions
+internal static class PointExtensions
 {
-    public static GridKey ToGridKey(this Point p)
+    public static Vector2 ToVector(this Point p)
     {
-        return new GridKey(p.X, p.Y);
+        return new Vector2(p.X, p.Y);
     }
 }
 
 public class VisitableArea
 {
-    private readonly HashSet<GridKey> _visited = [];
+    private readonly HashSet<Vector2> _visited = [];
 
     public int VisitedCount => _visited.Count;
 
     public void Visit(Point p)
     {
-        _visited.Add(p.ToGridKey());
+        _visited.Add(p.ToVector());
     }
 
     public bool IsVisited(Point p)
     {
-        return _visited.Contains(p.ToGridKey());
+        return _visited.Contains(p.ToVector());
     }
 }
--- Tests/Robot/Positioning/GridKeyExtensionsTest.cs
+++ Tests/Robot/Positioning/GridKeyExtensionsTest.cs
@@ -5,13 +5,13 @@ namespace Tests.Robot.Positioning;
 public class GridKeyExtensionsTest
 {
     [Fact]
-    public void ShouldMapPointToGridKeyProperly()
+    public void ShouldMapPointToVectorProperly()
     {
         const int x = 1;
         const int y = 2;
         var p = new Point(x, y);
 
-        var key = p.ToGridKey();
+        var key = p.ToVector();
 
         Assert.Equal(x, key.X);
         Assert.Equal(y, key.Y);
